52cb60f218c0a9cc5da771e481f803f1ae09d70a
hellow-world123 <2648243824@qq.com>
2025-06-25 16:27:10 +0800

    sez1

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+
 createApp(App).use(router).mount('#app')
