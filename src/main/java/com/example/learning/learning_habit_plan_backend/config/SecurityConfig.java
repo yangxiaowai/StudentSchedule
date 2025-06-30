f27afc84a2755022b57d691392d34f13ae385e44
liulianbuqu <2958637020@qq.com>
2025-06-30 10:26:12 +0800

    实现资料库账户分离（数据库有改动）

--- src/main/java/com/example/learning/learning_habit_plan_backend/config/SecurityConfig.java
+++ src/main/java/com/example/learning/learning_habit_plan_backend/config/SecurityConfig.java
@@ -95,7 +95,6 @@ public class SecurityConfig {
             )
             .authenticationProvider(authenticationProvider())
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-                .anyRequest().permitAll()
             );
 
         return http.build();
